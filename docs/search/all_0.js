85f546c509dd19753a8889a94f4d5b016b5a385a
Docs Robot <robot@example.com>
2023-03-16 13:07:06 +0000

    Committing documentation changes

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['addr_0',['addr',['../class_p_i_rsensor.html#a46a7f8821c4c0449ce63fb9f3a24dd31',1,'PIRsensor']]],
-  ['address_1',['address',['../class_easy_c.html#a6f4c6f2e8723b0c0495e126d49f5fcda',1,'EasyC']]],
-  ['available_2',['available',['../class_p_i_rsensor.html#a12effb16c33a8a0a4667b29189b37410',1,'PIRsensor']]]
+  ['address_0',['address',['../class_easy_c.html#a6f4c6f2e8723b0c0495e126d49f5fcda',1,'EasyC']]],
+  ['available_1',['available',['../class_p_i_rsensor.html#a12effb16c33a8a0a4667b29189b37410',1,'PIRsensor']]]
 ];
